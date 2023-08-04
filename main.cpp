8dbea3e942856c369eebcceef87bd1ec7dcb8629
ferrnnaando <fernindato@gmail.com>
2023-08-04 02:24:39 +0200

    print array

--- main.cpp
+++ main.cpp
@@ -10,8 +10,9 @@ using namespace std;
 int main() {
     int numero, contador = 0;
     do {
-        cout << "Introduce un numero:";
+        cout << "Introduce un numero: ";
         cin >> numero;
+
         if(numero >= 1){
             contador++;
             system("clear");
@@ -21,12 +22,31 @@ int main() {
     } while(numero != 0);
 
     if(contador > 1) {
-        cout <<"\nHas arruinado la cadena cuando llevabas " << contador << " valores introducidos correctamente.\n";
+        cout <<"Has arruinado la cadena cuando llevabas " << contador << " valores introducidos correctamente.\n";
     } else if (contador == 1) { 
-        cout <<"\nHas arruinado la cadena cuando llevabas " << contador << " valor introducido correctamente.\n";
+        cout <<"Has arruinado la cadena cuando llevabas " << contador << " valor introducido correctamente.\n";
      } else {
-        cout << "\nNo se ha guardado ningun numero porque no has introducido ninguno correctamente.\n";
+        cout << "No se ha guardado ningun numero porque no has introducido ninguno correctamente.\n";
      }
 
-        return 0;
+    double dynamic_double[] = {3.7, 2.9, 1.2, 1.1, 1.0, 392.5};
+    int thrD_array[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+    for(int a = 0; a < 3; a++) {
+        for(int b = 0; b < 3; b++) {
+            std::cout << thrD_array[a][b] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << std::endl;
+
+    for(const int (&row)[3] : thrD_array) {
+        for(const int &n : row) {
+            cout << n << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
     }
